ebb119e6e7f7941452f96d026340fd72954b3a07
I Chiu <93640984+winsyi531@users.noreply.github.com>
2025-09-03 16:46:29 +0800

    Update Train_Inference.sh

--- Train_Inference.sh
+++ Train_Inference.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 ##### 1 #####
-python train.py --n_layer 1 --bidirectional True &&
-python inference.py --n_layer 1 --bidirectional True &&
-python evaluate.py && 
-python train.py --n_layer 2 --bidirectional True &&
-python inference.py --n_layer 2 --bidirectional True &&
-python evaluate.py && 
-python train.py --n_layer 3 --bidirectional True &&
-python inference.py --n_layer 3 --bidirectional True &&
-python evaluate.py && 
-python train.py --n_layer 4 --bidirectional True &&
-python inference.py --n_layer 4 --bidirectional True &&
-python evaluate.py && 
 python train.py --n_layer 5 --bidirectional True &&
 python inference.py --n_layer 5 --bidirectional True &&
 python evaluate.py
